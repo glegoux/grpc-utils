103c1a8fe53b9a85aa21a7c71e05fe4a1b5bf9d6
glegoux <gilles.legoux@gmail.com>
2021-10-04 09:43:08 +0200

    Add comment for grpcui.sh

--- examples/scripts/grpcui.sh
+++ examples/scripts/grpcui.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
+#
+# Generate a gRPC UI available via the web browser
+#
+# Source code: https://github.com/fullstorydev/grpcui
+# Blog post: https://www.fullstory.com/blog/grpcui-dont-grpc-without-it/
 
-grpcui -open-browser=false -bind localhost -port 8080 -plaintext localhost:8000
+GRPC_UI_HOST='localhost'
+GRPC_UI_PORT='8080'
+GRPC_SERVER_ADR='localhost:8000'
+
+grpcui -open-browser=false -bind ${GRPC_UI_HOST} -port ${GRPC_UI_PORT} -plaintext ${GRPC_SERVER_ADR}
