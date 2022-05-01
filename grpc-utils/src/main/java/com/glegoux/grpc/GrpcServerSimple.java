ef38bcb740a8b2b23ba99bcf94eb122734f784d9
REVProfumo <33388181+REVProfumo@users.noreply.github.com>
2022-05-01 12:01:12 +0200

    Refactor GrprServer*
    - Change `logger` to `LOGGER`
    - Use `int` instead of `short` for port
    - Use `stop()` directly instead of `GrpcServerSimple.this.stop()`
    - Define new methods to factorize the code to parse an argument for a port
    -  Use `System.err.printf` instead of ` e.printStackTrace`
    
    /!\ Need to align coding style later

--- grpc-utils/src/main/java/com/glegoux/grpc/GrpcServerSimple.java
+++ grpc-utils/src/main/java/com/glegoux/grpc/GrpcServerSimple.java
@@ -14,40 +14,38 @@ import java.util.logging.Logger;
 
 public class GrpcServerSimple implements GrpcServer {
 
-    private static final Logger logger = Logger.getLogger(GrpcServerSimple.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(GrpcServerSimple.class.getName());
 
     private Server server;
     private HTTPServer monitoringServer;
 
     public void start(int port, int monitoringPort) throws IOException {
 
-        MonitoringServerInterceptor monitoringInterceptor =
-                MonitoringServerInterceptor.create(Configuration.allMetrics());
+        MonitoringServerInterceptor monitoringInterceptor = MonitoringServerInterceptor.create(Configuration.allMetrics());
 
         server = ServerBuilder.forPort(port)
-                .intercept(monitoringInterceptor)
-                .addService(new HealthStatusManager().getHealthService())
-                .addService(ProtoReflectionService.newInstance())
-                .build()
-                .start();
+            .intercept(monitoringInterceptor)
+            .addService(new HealthStatusManager().getHealthService())
+            .addService(ProtoReflectionService.newInstance())
+            .build()
+            .start();
 
-        logger.info("gRPC server started, listening on " + port);
+        LOGGER.info("gRPC server started, listening on " + port);
 
         monitoringServer = new HTTPServer(monitoringPort);
 
-        logger.info("Monitoring server for gRPC metrics started, listening on " + monitoringPort);
+        LOGGER.info("Monitoring server for gRPC metrics started, listening on " + monitoringPort);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-            System.err.println("shutting down gRPC server since JVM is shutting down");
+            System.err.println("Shutting down gRPC server since JVM is shutting down");
             try {
-                GrpcServerSimple.this.stop();
+                stop();
             } catch (InterruptedException e) {
-                e.printStackTrace(System.err);
+                System.err.printf("Server interrupted %s%n", e);
             }
             System.err.println("gRPC server shut down");
         }));
-
     }
 
     public void stop() throws InterruptedException {
@@ -67,8 +65,8 @@ public class GrpcServerSimple implements GrpcServer {
 
     public void run(String programName, String[] args) throws IOException, InterruptedException {
         GrpcServerSimpleArguments arguments = new GrpcServerSimpleArguments(programName, args);
-        short port = arguments.getPort();
-        short monitoringPort = arguments.getMonitoringPort();
+        int port = arguments.getPort();
+        int monitoringPort = arguments.getMonitoringPort();
         final GrpcServerSimple server = new GrpcServerSimple();
         server.start(port, monitoringPort);
         server.blockUntilShutdown();
--- grpc-utils/src/main/java/com/glegoux/grpc/GrpcServerSimpleArguments.java
+++ grpc-utils/src/main/java/com/glegoux/grpc/GrpcServerSimpleArguments.java
@@ -1,14 +1,24 @@
 package com.glegoux.grpc;
 
-import org.apache.commons.cli.*;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import java.util.logging.Logger;
 
 public class GrpcServerSimpleArguments {
-    private static final short DEFAULT_PORT = 8000;
-    private static final short DEFAULT_MONITORING_PORT = 8001;
+    private static final Logger LOGGER = Logger.getLogger(GrpcServerSimple.class.getName());
+    private static final int DEFAULT_PORT = 8000;
+    private static final int DEFAULT_MONITORING_PORT = 8001;
 
     private final String programName;
-    private short port = DEFAULT_PORT;
-    private short monitoringPort = DEFAULT_MONITORING_PORT;
+    private int port = DEFAULT_PORT;
+    private int monitoringPort = DEFAULT_MONITORING_PORT;
 
     public GrpcServerSimpleArguments(String programName, String[] args) {
         this.programName = programName;
@@ -27,7 +37,7 @@ public class GrpcServerSimpleArguments {
                 this.monitoringPort = checkAndGetPort(cmd, "monitoring-port");
             }
         } catch (ParseException exception) {
-            System.err.println(exception.getMessage());
+            LOGGER.warning(String.format("Incorrect server arguments %s", exception));
             System.exit(1);
         }
     }
@@ -36,11 +46,11 @@ public class GrpcServerSimpleArguments {
         return programName;
     }
 
-    public short getPort() {
+    public int getPort() {
         return port;
     }
 
-    public short getMonitoringPort() {
+    public int getMonitoringPort() {
         return monitoringPort;
     }
 
@@ -53,42 +63,39 @@ public class GrpcServerSimpleArguments {
     private Options buildOptions() {
         Options options = new Options();
         Option help = new Option("h", "help", false, "print this message");
-        Option port = Option.builder("p").longOpt("port")
-                .hasArg()
-                .required(false)
-                .type(Short.class)
-                .argName("value")
-                .desc(String.format("gRPC server port (default: %d)", DEFAULT_PORT))
-                .build();
-        Option monitoringPort = Option.builder().longOpt("monitoring-port")
-                .hasArg()
-                .required(false)
-                .type(Short.class)
-                .argName("value")
-                .desc(
-                        String.format(
-                                "monitoring server port for prometheus exporter of gRPC metrics (default: %d)",
-                                DEFAULT_MONITORING_PORT
-                        )
-                )
-                .build();
+        Option port = getPort("port", String.format("gRPC server port (default: %d)", DEFAULT_PORT));
+        Option monitoringPort = getPort("monitoring-port",
+            String.format("monitoring server port for prometheus exporter of gRPC metrics (default: %d)",
+            DEFAULT_MONITORING_PORT));
         options.addOption(help);
         options.addOption(port);
         options.addOption(monitoringPort);
         return options;
     }
 
-    private short checkAndGetPort(CommandLine cmd, String longOpt) throws ParseException {
-        Object port = cmd.getParsedOptionValue(longOpt);
-        if (port == null || !(1024 <= (Short) port && (Short) port <= 65535)) {
-            throw new ParseException(
-                    String.format(
-                            "Failed to get the value `%s` of the option --%s must be a integer between 1024 and 65535",
-                            cmd.getOptionValue(longOpt),
-                            longOpt
-                    )
-            );
-        }
-        return (Short) port;
+    private Option getPort(String name, String desc) {
+        return Option.builder().longOpt(name)
+            .hasArg()
+            .required(false)
+            .type(Integer.class)
+            .argName("value")
+            .desc(desc)
+            .build();
+    }
+
+    private int checkAndGetPort(CommandLine cmd, String longOpt) throws ParseException {
+        try {
+            Integer port = (Integer) cmd.getParsedOptionValue(longOpt);
+            if (port == null || !(1024 <= port && port <= 65535))
+                throw new ParseException(getExceptionMsg(cmd, longOpt));
+        } catch (ClassCastException e) {
+            throw new ParseException(getExceptionMsg(cmd, longOpt));        }
+        return port;
+    }
+
+    private String getExceptionMsg(CommandLine cmd, String longOpt) {
+        return String.format(
+            "Failed to get the value `%s` of the option --%s must be an integer between 1024 and 65535",
+            cmd.getOptionValue(longOpt), longOpt);
     }
 }
