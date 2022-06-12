c431b8ca761675df073b532162cccfe6ebd64e10
REVProfumo <33388181+REVProfumo@users.noreply.github.com>
2022-06-12 14:38:02 +0200

    Add IT for HelloService (#9)
    Co-authored-by: re.profumo <re.profumo@criteo.com>

--- examples/src/main/java/com/glegoux/grpc/examples/hello/service/HelloService.java
+++ examples/src/main/java/com/glegoux/grpc/examples/hello/service/HelloService.java
@@ -6,11 +6,16 @@ import com.glegoux.examples.hello.HelloServiceGrpc;
 import io.grpc.stub.StreamObserver;
 
 public class HelloService extends HelloServiceGrpc.HelloServiceImplBase {
+
     @Override
     public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+        HelloReply reply = HelloReply.newBuilder().setMessage(hello(req.getName())).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
+    }
+
+    static String hello(String name) {
+        return "Hello " + name;
     }
 
 }
