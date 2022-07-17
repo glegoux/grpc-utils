ba0dea0dc69dcd91537adc91b65a134b2cc60e9c
Gilles Legoux <gilles.legoux@gmail.com>
2022-07-18 00:32:44 +0200

    Split examples for hello into 3 modules: proto, client and server

--- examples/hello/client/src/main/java/com/glegoux/grpc/examples/hello/client/HelloClient.java
+++ examples/hello/client/src/main/java/com/glegoux/grpc/examples/hello/client/HelloClient.java
@@ -3,17 +3,17 @@ package com.glegoux.grpc.examples.hello.client;
 import com.glegoux.examples.hello.HelloReply;
 import com.glegoux.examples.hello.HelloRequest;
 import com.glegoux.examples.hello.HelloServiceGrpc;
+import com.glegoux.grpc.client.config.GrpcClientServiceConfig;
 import io.grpc.ManagedChannel;
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class HelloClient {
 
     public static void main(String[] args) {
         NettyChannelBuilder channelBuilder = NettyChannelBuilder.forAddress("localhost", 8000);
-        Map<String, ?> serviceConfig = new HashMap<>(); //;GrpcClientServiceConfig.build();
+        Map<String, ?> serviceConfig = GrpcClientServiceConfig.build();
         channelBuilder.defaultServiceConfig(serviceConfig).defaultLoadBalancingPolicy("").usePlaintext();
         ManagedChannel channel = channelBuilder.build();
         HelloServiceGrpc.HelloServiceBlockingStub blockingClient = HelloServiceGrpc.newBlockingStub(channel);
